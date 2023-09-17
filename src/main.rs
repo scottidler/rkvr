33edc023ce05d677e116d86a712a2c8f9978f8a5
Scott A. Idler <scott.a.idler@gmail.com>
2023-09-17 12:47:10 -0700

    after some fixups

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,28 @@
-#![allow(dead_code)]
+#![cfg_attr(debug_assertions, allow(unused_imports, unused_variables, unused_mut, dead_code))]
 
-use clap::{Parser, Subcommand};
-use dirs;
-use std::fs;
-use std::fs::File;
-use std::path::{Path,PathBuf};
+// Standard library imports
+use std::fs::{self, File, OpenOptions};
+use std::io::prelude::*;
+use std::os::unix::fs::PermissionsExt;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
 use std::time::SystemTime;
-use configparser::ini::Ini;
-use std::io::prelude::*;
-use std::fs::OpenOptions;
 
-use std::os::unix::fs::PermissionsExt;
-use libc::S_IRUSR;
-use libc::S_IWUSR;
-use libc::S_IXUSR;
-use libc::S_IRGRP;
-use libc::S_IWGRP;
-use libc::S_IXGRP;
-use libc::S_IROTH;
-use libc::S_IWOTH;
-use libc::S_IXOTH;
+// Third-party crate imports
+use chrono::{Duration, TimeZone, Utc};
+use clap::{Parser, Subcommand};
+use configparser::ini::Ini;
+use dirs;
+use eyre::{eyre, Context, Result};
+use walkdir::WalkDir;
 
 #[derive(Parser)]
-#[command(
-    author = "Scott A. Idler",
-    version = "0.1",
-    about = "tool for staging rmrf-ing or bkup-ing files",
-    long_about = None)]
+#[command(name = "rmrf", about = "tool for staging rmrf-ing or bkup-ing files")]
+#[command(version = "0.1.0")]
+#[command(author = "Scott A. Idler <scott.a.idler@gmail.com>")]
+#[command(after_help = "after_help")]
+#[command(arg_required_else_help = true)]
 struct Cli {
-    /*
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file:w
-    ///
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-    */
     #[arg(name = "targets")]
     targets: Vec<String>,
 
@@ -60,31 +41,36 @@ enum Action {
     BkupRmrf,
 }
 // make a name from the basename of the path
-fn make_name(path: &Path) -> String {
-    let mut name = path.file_name().unwrap().to_str().unwrap().to_owned();
-    name = name.replace("/", "-");
-    name = name.replace(":", "_");
-    name = name.replace(" ", "_");
-    name
+fn make_name(path: &Path) -> Result<String> {
+    let name = path
+        .file_name()
+        .ok_or_else(|| eyre!("Failed to get file name"))?
+        .to_str()
+        .ok_or_else(|| eyre!("Failed to convert to str"))?
+        .to_owned()
+        .replace("/", "-")
+        .replace(":", "_")
+        .replace(" ", "_");
+
+    Ok(name)
 }
 
-fn execute<T: AsRef<str>>(sudo: bool, args: &[T]) -> Output {
-    let mut args: Vec<String> = args.iter().map(|arg| arg.as_ref().to_owned()).collect();
+fn execute<T: AsRef<str>>(sudo: bool, args: &[T]) -> Result<Output> {
+    let mut command = Command::new(if sudo { "sudo" } else { args[0].as_ref() });
+
     if sudo {
-        args.insert(0, "sudo".to_owned());
+        command.arg(args[0].as_ref());
     }
-    let mut command = Command::new(args[0].clone());
-    for arg in &args[1..] {
-        command.arg(arg);
+
+    for arg in args[1..].iter() {
+        command.arg(arg.as_ref());
     }
-    command.output().expect("failed to execute process")
+
+    command.output().map_err(|e| eyre!(e))
 }
 
-fn main() {
-    let timestamp = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+fn main() -> Result<()> {
+    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs();
 
     let matches = Cli::parse();
     let action: Action = matches.action.unwrap_or_default();
@@ -95,258 +81,182 @@ fn main() {
         .map(|f| PathBuf::from(f))
         .collect();
 
-    /*
-    ❯ bat -p .config/rmrf/rmrf2.cfg
-
-    #where to store tarballs of removed files
-    RMRF_PATH=/var/tmp/rmrf2
-
-    #use sudo by default
-    SUDO=yes
-
-    #how many days to keep files in tarball at /var/tmp/rmrf
-    KEEPALIVE=21
-
-     */
-    let rmrf_cfg_path = dirs::home_dir().unwrap().join(".config/rmrf/rmrf2.cfg");
+    let rmrf_cfg_path = dirs::home_dir()
+        .ok_or(eyre!("home dir not found!"))?
+        .join(".config/rmrf/rmrf2.cfg");
     let mut rmrf_cfg = Ini::new();
-    rmrf_cfg.load(&rmrf_cfg_path).unwrap();
+    rmrf_cfg
+        .load(&rmrf_cfg_path)
+        .map_err(|e| eyre!(e))
+        .wrap_err("Failed to load config")?;
     let path = rmrf_cfg.get("DEFAULT", "path").unwrap_or("/var/tmp/rmrf".to_owned());
     let sudo: bool = rmrf_cfg.get("DEFAULT", "sudo").unwrap_or("yes".to_owned()) == "yes";
-    let keep: i32 = rmrf_cfg.get("DEFAULT", "keep").unwrap_or("21".to_owned()).parse().unwrap();
+    let days: i32 = rmrf_cfg.get("DEFAULT", "keep").unwrap_or("21".to_owned()).parse()?;
 
     let rmrf_path = Path::new(&path);
     let bkup_path = Path::new(&rmrf_path).join("bkup");
 
-    fs::create_dir_all(&rmrf_path).unwrap();
-    fs::create_dir_all(&bkup_path).unwrap();
+    fs::create_dir_all(&rmrf_path)?;
+    fs::create_dir_all(&bkup_path)?;
 
     for target in targets.into_iter() {
         match action {
-            Action::Bkup => archive(&bkup_path, timestamp.to_string(), &target, sudo, None),
-            Action::Rmrf => archive(&rmrf_path, timestamp.to_string(), &target, sudo, Some(keep)),
+            Action::Bkup => archive(&bkup_path, timestamp.to_string(), &target, sudo, false, None)?,
+            Action::Rmrf => archive(&rmrf_path, timestamp.to_string(), &target, sudo, true, Some(days))?,
             Action::LsBkup => println!("ls bkup"),
             Action::LsRmrf => println!("ls rmrf"),
             Action::BkupRmrf => println!("bkup rmrf"),
         }
     }
+
+    Ok(())
 }
 
-fn archive(path: &Path, timestamp: String, target: &Path, sudo: bool, keep: Option<i32>) {
-    let name = make_name(target);
+fn cleanup(dir_path: &std::path::Path, days: usize) -> Result<()> {
+    let now = SystemTime::now();
+
+    // Read the directory
+    let entries = fs::read_dir(dir_path)?;
+
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let metadata = fs::metadata(&path)?;
+
+            // Get the modified time as a SystemTime
+            let modified_time = metadata.modified()?;
+
+            // Calculate the duration since the file was last modified
+            if let Ok(duration_since_modified) = now.duration_since(modified_time) {
+                // Convert days to duration and compare
+                if duration_since_modified > std::time::Duration::from_secs(60 * 60 * 24 * days as u64) {
+                    // Delete the file or directory
+                    if metadata.is_dir() {
+                        fs::remove_dir_all(&path)?;
+                    } else {
+                        fs::remove_file(&path)?;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn archive(path: &Path, timestamp: String, target: &Path, sudo: bool, remove: bool, keep: Option<i32>) -> Result<()> {
+    println!(
+        "archive: path={} timestamp={} target={}",
+        path.to_string_lossy(),
+        timestamp,
+        target.to_string_lossy()
+    );
+    let name = make_name(target)?;
     println!("name={}", name);
     let base = path.join(&timestamp);
     println!("base={}", base.to_string_lossy());
-    execute(false, &vec!["mkdir", "-p", base.to_str().unwrap()]);
+    execute(false, &vec!["mkdir", "-p", base.to_str().unwrap()])?;
 
     if target == &base {
         println!("{} ->", path.to_string_lossy());
-        let output = execute(sudo, &vec![
-            "tar",
-            "--absolute-names",
-            "-xvf",
-            target.to_str().unwrap(),
-        ]);
+        let output = execute(
+            sudo,
+            &vec![
+                "tar",
+                "--absolute-names",
+                "-xvf",
+                target.to_str().ok_or(eyre!("Failed to convert path to string"))?,
+            ],
+        )?;
         print!("  {}", String::from_utf8_lossy(&output.stdout));
-        return;
+        return Ok(());
     }
     let tarball = format!("{}.tar.gz", name);
     let metadata = format!("{}.meta", name);
 
-    let output = if path.join(target).metadata().unwrap().is_dir() {
-        execute(sudo, &vec![
-            "tree",
-            "-a",
-            "-h",
-            "-L",
-            "2",
-            path.join(target).to_str().unwrap(),
-        ])
+    let tree_path_buf = path.join(target);
+    let tree_path = tree_path_buf.to_str().ok_or(eyre!("Couldn't run tree command"))?;
+
+    let ls_path_buf = path.join(target);
+    let ls_path = ls_path_buf.to_str().ok_or(eyre!("Couldn't run ls command"))?;
+
+    let output = if path.join(target).metadata()?.is_dir() {
+        execute(sudo, &vec!["tree", "-a", "-h", "-L", "2", tree_path])?
     } else {
-        execute(sudo, &vec![
-            "ls",
-            "-alh",
-            path.join(target).to_str().unwrap(),
-        ])
+        execute(sudo, &vec!["ls", "-alh", ls_path])?
     };
 
     let metadata_path = base.join(&metadata);
     println!("metadata_path={}", metadata_path.to_string_lossy());
     File::create(&metadata_path).expect("create file failed");
 
-    let mut file_permissions = fs::metadata(&metadata_path)
-        .unwrap()
-        .permissions();
-    file_permissions.set_mode(S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-    fs::set_permissions(&metadata_path, file_permissions).unwrap();
+    let mut perms = fs::metadata(&metadata_path)?.permissions();
+    perms.set_mode(0o755); // Use octal notation for permissions
+    fs::set_permissions(&metadata_path, perms)?;
 
-    let mut metadata_file = OpenOptions::new()
-        .write(true)
-        .open(metadata_path)
-        .unwrap();
-    metadata_file.write_all(&output.stdout).unwrap();
+    let mut metadata_file = OpenOptions::new().write(true).open(metadata_path)?;
+    metadata_file.write_all(&output.stdout)?;
 
-    let output = execute(sudo, &vec![
-        "tar",
-        "--absolute-names",
-        "--preserve-permissions",
-        "-cvzf",
-        &tarball,
-        path.join(target).to_str().unwrap(),
-    ]);
+    let output = execute(
+        sudo,
+        &vec![
+            "tar",
+            "--absolute-names",
+            "--preserve-permissions",
+            "-cvzf",
+            &tarball,
+            path.join(target)
+                .to_str()
+                .ok_or(eyre!("Failed to convert path to string"))?,
+        ],
+    )?;
+
     print!("  {}", String::from_utf8_lossy(&output.stdout));
 
     let new_tarball_path = base.join(&tarball);
-    fs::rename(&tarball, &new_tarball_path).unwrap();
+    fs::rename(&tarball, &new_tarball_path)?;
     println!("-> {}", new_tarball_path.to_string_lossy());
 
-    if let Some(keep_days) = keep {
-        let deleted = execute(sudo, &vec![
-            "find",
-            base.to_string_lossy().as_ref(),
-            "-mtime",
-            &format!("+{}", keep_days),
-            "-type",
-            "d",
-            "-print",
-        ]);
-        let deleted_dirs = String::from_utf8_lossy(&deleted.stdout);
+    if let Some(days) = keep {
+        let output = execute(
+            sudo,
+            &vec![
+                "find",
+                base.to_string_lossy().as_ref(),
+                "-mtime",
+                &format!("+{}", days),
+                "-type",
+                "d",
+                "-print",
+            ],
+        )?;
+        let deleted_dirs = String::from_utf8_lossy(&output.stdout);
         if !deleted_dirs.trim().is_empty() {
             println!("{}", deleted_dirs);
             println!("-> /dev/null");
         }
-        execute(sudo, &vec![
-            "find",
-            base.to_string_lossy().as_ref(),
-            "-mtime",
-            &format!("+{}", keep_days),
-            "-type",
-            "d",
-            "-delete",
-        ]);
-        execute(sudo, &vec![
-            "rm",
-            "-rf",
-            target.to_str().unwrap(),
-        ]);
+        execute(
+            sudo,
+            &vec![
+                "find",
+                base.to_string_lossy().as_ref(),
+                "-mtime",
+                &format!("+{}", days),
+                "-type",
+                "d",
+                "-delete",
+            ],
+        )?;
     }
 
-    match keep {
-        Some(days) => {
-
-        },
-        None => {
-            execute(sudo, &vec![
-                "rm",
-                "-rf",
-                target.to_str().unwrap(),
-            ]);
-        },
+    let rmrf_path = target.to_str().ok_or(eyre!("Error running rm -rf"))?;
+    if remove {
+        println!("rm -rf {}", target.to_string_lossy());
+        execute(sudo, &vec!["rm", "-rf", rmrf_path])?;
     }
+
+    Ok(())
 }
-
-// fn acrhive(path: &Path, timestamp: String, target: &String, sudo: bool, keep: Option<i32>) {
-//     let name = make_name(&target);
-//     println!("name={}", name);
-//     // create a path based upon path / timestamp
-//     // name.tar.gz will be the archive name located at that ^^^ path
-//     // mame.meta will be a file containing either the ls of the file or tree of a directory
-//     let base = path.join(timestamp);
-//     if target == base.to_string_lossy().into_owned() {
-//         println!("{} ->", path);
-//         let output = execute(sudo, &vec![
-//             "tar",
-//             "--absolute-names",
-//             "-xvf",
-//             &target]);
-//         print!("  {}", String::from_utf8_lossy(&output.stdout));
-//         return;
-//     }
-//     let tarball = format!("{name}.tar.gz");
-//     let metadata = format!("{name}.meta");
-
-//     let output = if fs::metadata(&path).unwrap().is_dir() {
-//         execute(sudo, &vec![
-//             "tree",
-//             "-a",
-//             "-h",
-//             "-L",
-//             "2",
-//             &path])
-//     } else {
-//         execute(sudo, &vec![
-//             "ls",
-//             "-alh",
-//             &path])
-//     };
-//     let mut metadata_file = File::create(path.join(metadata)).unwrap();
-//     metadata_file.write_all(&output.stdout).unwrap();
-
-//     let output = execute(sudo, &vec![
-//         "tar",
-//         "--absolute-names",
-//         "--preserve-permissions",
-//         "-cvzf",
-//         &tarball,
-//         &path]);
-//     print!("  {}", String::from_utf8_lossy(&output.stdout));
-
-//     // move the tarball and metadata file to the base path
-//     if let Some(days) = keep {
-//         // print days that will be deleted
-//         let output = execute(sudo, &vec!["find", base.to_string_lossy().as_ref(), "-mtime", &days.to_string(), "-type", "d", "-print"]);
-//         print!("  {}", String::from_utf8_lossy(&output.stdout));
-//     }
-// }
-
-
-// fn archive2(dir_path: &Path, sudo: bool, files: &[String], tarball: &str, keep: Option<i32>) {
-//     if files[0].starts_with(dir_path.to_string_lossy().as_ref()) {
-//         println!("{} ->", files[0]);
-//         // map archive to vec of strings, chain with files
-//         let args: Vec<String> = EXTRACT
-//             .iter()
-//             .map(|s| s.to_string())
-//             .chain(files.iter().cloned())
-//             .collect::<Vec<String>>();
-//         let output = execute(sudo, &args);
-//         let output = execute(sudo, vec!["tar", "--"])
-//         print!("  {}", String::from_utf8_lossy(&output.stdout));
-//         return;
-//     }
-
-//     let tarball_path = dir_path.join(tarball);
-
-//     // map archive to vec of strings, chain the tarball and the files
-//     let args: Vec<String> = ARCHIVE
-//         .iter()
-//         .map(|s| s.to_string())
-//         .chain(vec![tarball_path.to_string_lossy().into_owned()])
-//         .chain(files.iter().cloned())
-//         .collect::<Vec<String>>();
-//     let output = execute(sudo, &args);
-//     println!("{}", String::from_utf8_lossy(&output.stdout));
-
-//     println!("-> {}", tarball_path.to_string_lossy());
-
-//     if let Some(keep) = keep {
-//         let deleted = find_files_older_than(dir_path, keep);
-//         if !deleted.is_empty() {
-//             for path in &deleted {
-//                 println!("{}", path);
-//             }
-//             println!("-> /dev/null");
-//         }
-
-//         for path in deleted {
-//             fs::remove_file(path).unwrap();
-//         }
-
-//         for file in files {
-//             fs::remove_file(file).unwrap();
-//         }
-//     }
-// }
 
 fn list(dir_path: &Path) {
     let tarballs = find_files_with_extension(dir_path, "tar.gz");
@@ -370,14 +280,14 @@ fn list(dir_path: &Path) {
     println!("{:?} {}K", dir_path, size / 1024);
 }
 
-fn find_files_with_extension(dir_path: &Path, extension: &str) -> Vec<String> {
+fn find_files_with_extension(dir_path: &Path, ext: &str) -> Vec<String> {
     let mut result = vec![];
 
     if let Ok(entries) = fs::read_dir(dir_path) {
         for entry in entries {
             if let Ok(entry) = entry {
                 let path = entry.path();
-                if path.is_file() && path.extension().unwrap() == extension {
+                if path.is_file() && path.extension().unwrap() == ext {
                     result.push(path.to_string_lossy().into_owned());
                 }
             }
@@ -387,7 +297,7 @@ fn find_files_with_extension(dir_path: &Path, extension: &str) -> Vec<String> {
     result
 }
 
-fn find_files_older_than(dir_path: &Path, days: i32) -> Vec<String> {
+fn find_files_older_than(dir_path: &Path, days: usize) -> Vec<String> {
     let now = SystemTime::now();
     let duration = std::time::Duration::from_secs((days * 24 * 60 * 60) as u64);
     let cutoff = now - duration;
